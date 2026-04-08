ef4c42b8dcde281020c4aa56f5e7af428a6e0dd5
Kanishk Sasikumar <102179266+Kanishksasi@users.noreply.github.com>
2026-04-08 10:10:30 -0500

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,16 @@
+"""
+introduction.py
+
+The first introduction course on github from CSHS. To complete this demo test, fill out the variable on line 10
+with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+"""
+
+def main():
+    print("Hello World!")
+
+    name = "" # TODO: Insert your name between the double quotes
+
+    print(f"{name}, Welcome to the CSHS!")
+
+if __name__ == "__main__":
+    main()
